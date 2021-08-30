004918f1cd288c6f0e1673b07976358f7562f787
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-08-30 04:05:29 +0100

    Feature: Created setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center gap-y-4 h-screen bg-gray-100">
+      <img src={logo} className="h-16 w-auto" alt="logo" />
+      <h1 className="text-5xl">React Starter Template With Typescript</h1>
+    </div>
+  );
+}
+
+export default App;
